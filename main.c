3b16ed0251b9910e23398edb524a0f444cc0d4ea
DudewithPigskin <Koenpeelen@hotmail.com>
2016-06-09 15:37:45 +0200

    Add files via upload
    Body. Comments zeggen wat waar moet, enjoy lieverds!

---
+++ main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+/*enter all variables*/
+
+/*create empty map*/
+
+/*boot up communication and test once*/
+
+/*fill in the 3 points*/
+
+/*fill map*/
+
+/*plan route*/
+
+/*fill array*/
+
+while(1){
+    /*send next direction*/
+
+    /*confirm order*/
+    /*is there a mine?*/
+    if(/*yes*/){
+        /*safe mine location*/
+
+        /*safe own new location*/
+
+        /*plan route*/
+
+        /*fill array*/
+
+        /*reset parameter*/
+
+        break
+    }/*end mine-if*/
+    else/*no mine is found*/{
+        /*safe location*/
+
+        if(/*location is end*/) {
+            /*end program*/
+
+        }/*end end-if*/
+        /*add one to parameter*/
+
+        break
+        }/*end no mine_if*/
+    }/*end inf-while loop*/
+}/*end main*/
